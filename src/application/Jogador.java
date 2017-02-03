4de8d22294a8c2b852cf3034b9229dc3a98f35ee
Andre <Andre@Andre-PC>
2017-02-03 15:50:13 -0200

    Mapeamento das coordenadas de todos (2 por enquanto) os jogadores

--- src/application/Jogador.java
+++ src/application/Jogador.java
@@ -1,24 +1,27 @@
 package application;
 
-
 import javafx.scene.Group;
-
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Jogador {
 	
 	public static void jogadores(int numJogadores, Group root){
 		
+		model.Jogador j = new model.Jogador();
+		
 		Circle player1 = new Circle(); 
 		player1.setRadius(5.0);
-		player1.setTranslateY(360);
-		player1.setTranslateX(540);
+		player1.setFill(Color.DARKORANGE);
+		player1.setTranslateY(j.getYJogador(0, 1));
+		player1.setTranslateX(j.getXJogador(0, 1));
 		player1.setCache(true);
 	    
 	    Circle player2 = new Circle(); 
 	    player2.setRadius(5.0);
-	    player2.setTranslateY(345);
-	    player2.setTranslateX(540);
+	    player2.setFill(Color.CHARTREUSE);
+	    player2.setTranslateY(j.getYJogador(1, 13));
+	    player2.setTranslateX(j.getXJogador(1, 13));
 	    player2.setCache(true);
 	    
 	    Circle player3 = new Circle(); 
